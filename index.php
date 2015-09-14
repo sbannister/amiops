c5bbad84cf93d8e01b0b744d4c7c01f7ff308222
sbannister <sbannister@powercor.com.au>
2015-09-14 15:02:34 +1000

    Update index.php
    Added select sql

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+
 <?    try
     {
         $connectionInfo = array("UID" => "sbannister@w8bvrrkcp9", "pwd" => "T@maz0n13", "Database" => "AmiAdvantageDb", "LoginTimeout" => 30, "Encrypt" => 1);
@@ -10,12 +16,28 @@
     {
         echo("Error!");
     }
+
+        $conn = OpenConnection();
+        $tsql = "SELECT TOP 100 * FROM device";
+        $getProducts = sqlsrv_query($conn, $tsql);
+        if ($getProducts == FALSE)
+            die(FormatErrors(sqlsrv_errors()));
+        $productCount = 0;
+        while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))
+        {
+            echo($row['Device_Serial_No']);
+            echo("<br/>");
+            $productCount++;
+        }
+        sqlsrv_free_stmt($getProducts);
+        sqlsrv_close($conn);
+    }
+    catch(Exception $e)
+    {
+        echo("Error!");
+    }
+    
 ?>
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
+
  </body>
 </html>
