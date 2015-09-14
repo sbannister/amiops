16ea04f896c2867a59f808c45b3d3dae9e9aa206
sbannister <sbannister@powercor.com.au>
2015-09-14 14:59:36 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-    try
+<?    try
     {
         $connectionInfo = array("UID" => "sbannister@w8bvrrkcp9", "pwd" => "T@maz0n13", "Database" => "AmiAdvantageDb", "LoginTimeout" => 30, "Encrypt" => 1);
         $serverName = "tcp:w8bvrrkcp9.database.windows.net,1433";
@@ -10,7 +10,7 @@
     {
         echo("Error!");
     }
-
+?>
 <html>
  <head>
   <title>PHP Test</title>
