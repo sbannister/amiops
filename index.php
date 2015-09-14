8b3889e97d4ce6e398f4b2f0ef697e5d6a10115d
sbannister <sbannister@powercor.com.au>
2015-09-14 14:58:44 +1000

    Update index.php
    Added database connection

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+    try
+    {
+        $connectionInfo = array("UID" => "sbannister@w8bvrrkcp9", "pwd" => "T@maz0n13", "Database" => "AmiAdvantageDb", "LoginTimeout" => 30, "Encrypt" => 1);
+        $serverName = "tcp:w8bvrrkcp9.database.windows.net,1433";
+        $conn = sqlsrv_connect($serverName, $connectionInfo);
+        if($conn == false)
+            die(FormatErrors(sqlsrv_errors()));
+    }
+    catch(Exception $e)
+    {
+        echo("Error!");
+    }
+
 <html>
  <head>
   <title>PHP Test</title>
