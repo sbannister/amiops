d3c85be6161f66b9834713af1bd40271a920c49b
sbannister <sbannister@powercor.com.au>
2015-09-14 17:01:15 +1000

    Update getMap.php

--- getMap.php
+++ getMap.php
@@ -1,6 +1,10 @@
-<?
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
 
-  try
+<?    try
     {
         $connectionInfo = array("UID" => "sbannister@w8bvrrkcp9", "pwd" => "T@maz0n13", "Database" => "AmiAdvantageDb", "LoginTimeout" => 30, "Encrypt" => 1);
         $serverName = "tcp:w8bvrrkcp9.database.windows.net,1433";
@@ -12,38 +16,28 @@
     {
         echo("Error!");
     }
-echo("Here!");
 try
     {
         $tsql = "SELECT TOP 100 * FROM location";
-        $result = sqlsrv_query($conn, $tsql);
-        if ($result == FALSE)
+        $getProducts = sqlsrv_query($conn, $tsql);
+        if ($getProducts == FALSE)
             die(FormatErrors(sqlsrv_errors()));
+        $productCount = 0;
+        while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))
+        {
+            echo($row['Device_Util_ID']);
+            echo("<br/>");
+            $productCount++;
+        }
+        sqlsrv_free_stmt($getProducts);
+        sqlsrv_close($conn);
     }
     catch(Exception $e)
     {
         echo("Error!");
     }
-
-header("Content-type: text/xml");
-
-// Start XML file, echo parent node
-echo '<markers>';
-
-// Iterate through the rows, printing XML nodes for each
-while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
-{
-  // ADD TO XML DOCUMENT NODE
-  echo '<marker ';
-  echo 'name="' .$row['Device_Util_ID'] . '" ';
-  echo 'lat="' . $row['Service_Pt_Latitude'] . '" ';
-  echo 'lng="' . $row['Service_Pt_Longitude'] . '" ';
-  echo '/>';
-}
-
-// End XML file
-echo '</markers>';
-
-sqlsrv_free_stmt($result);
-sqlsrv_close($conn);
+    
 ?>
+
+ </body>
+</html>
